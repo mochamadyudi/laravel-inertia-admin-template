cc8645427325abf11c7e50944df8b2ffc59bd0f2
mochamadyudi <myudisobari12@gmail.com>
2025-01-24 00:05:15 +0700

    Fix missing style

---
+++ app/Helpers/menu.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+  'dashboard' => [
+    [
+//      'key'=> route('dashboard.index'),
+      'type' => 'item', //
+      'label' => 'Dashboard',
+      'iconType' => 'antd', // 'antd' | 'font-awesome' | html
+      'icon' => '',
+      'roles' => [],
+      'permissions'=> [],
+    ]
+  ]
+];
