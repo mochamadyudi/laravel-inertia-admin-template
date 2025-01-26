69f0f03afefc6fbceec5195ba1355fc566b408a4
mochamadyudi <myudisobari12@gmail.com>
2025-01-26 20:14:31 +0700

    create profile page pages

---
+++ resources/js/Pages/Dashboard/Pages/App/profile.page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import TheDashboard from "@/Components/layouts/Dashboard/TheDashboard";
+
+const Page = () => {
+  return (
+    <React.Fragment>
+
+    </React.Fragment>
+  )
+}
+
+Page.layout = (children: React.ReactNode) => <TheDashboard children={children}/>;
+
+export default Page;
