346fee3277f1da4915ccaad72f41a0b63b089b2d
mochamadyudi <myudisobari12@gmail.com>
2025-01-26 14:23:04 +0700

    [done] page components typography.

--- resources/js/Pages/Dashboard/Components/Generals/typography/partials/ellipsis-suffix.partial.tsx
+++ resources/js/Pages/Dashboard/Components/Generals/typography/partials/ellipsis-suffix.partial.tsx
@@ -1,8 +1,5 @@
 import React, { useState } from 'react';
-import {Card, Slider, Typography} from 'antd';
-//@ts-ignore
-import MD, { attributes } from "@/Pages/Dashboard/Components/Generals/typography/markdown/EllipsisSuffix.md";
-import DemoCard from "@/Components/util-component/DemoCard";
+import {Slider, Typography} from 'antd';
 
 const { Paragraph } = Typography;
 
@@ -13,22 +10,22 @@ const EllipsisSuffix: React.FC = () => {
     "To be, or not to be, that is the question: Whether it is nobler in the mind to suffer. The slings and arrows of outrageous fortune Or to take arms against a sea of troubles, And by opposing end them? To die: to sleep; No more; and by a sleep to say we end The heart-ache and the thousand natural shocks That flesh is heir to, 'tis a consummation Devoutly to be wish'd. To die, to sleep To sleep- perchance to dream: ay, there's the rub! For in that sleep of death what dreams may come When we have shuffled off this mortal coil, Must give us pause. There 's the respect That makes calamity of so long life";
 
   return (
-    <DemoCard source={<MD/>} attributes={attributes}>
-      <Slider value={rows} min={1} max={10} onChange={setRows} />
-      <Paragraph
-        ellipsis={{
-          rows,
-          expandable: true,
-          suffix: '--William Shakespeare',
-          onEllipsis: (ellipsis) => {
-            console.log('Ellipsis changed:', ellipsis);
-          },
-        }}
-        title={`${article}--William Shakespeare`}
-      >
-        {article}
-      </Paragraph>
-    </DemoCard>
+      <React.Fragment>
+        <Slider value={rows} min={1} max={10} onChange={setRows} />
+        <Paragraph
+          ellipsis={{
+            rows,
+            expandable: true,
+            suffix: '--William Shakespeare',
+            onEllipsis: (ellipsis) => {
+              console.log('Ellipsis changed:', ellipsis);
+            },
+          }}
+          title={`${article}--William Shakespeare`}
+        >
+          {article}
+        </Paragraph>
+      </React.Fragment>
   );
 };
 
