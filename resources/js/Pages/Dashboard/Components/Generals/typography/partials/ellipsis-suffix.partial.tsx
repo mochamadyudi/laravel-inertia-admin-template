11f02863f6d5699d440cb92234b7d6dd8f582adc
mochamadyudi <myudisobari12@gmail.com>
2025-01-26 20:14:19 +0700

    [fix] page component typography

--- resources/js/Pages/Dashboard/Components/Generals/typography/partials/ellipsis-suffix.partial.tsx
+++ resources/js/Pages/Dashboard/Components/Generals/typography/partials/ellipsis-suffix.partial.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import {Slider, Typography} from 'antd';
+import DemoCard from "@/Components/util-component/DemoCard";
+// @ts-ignore
+import MD, { attributes } from '../markdown/EllipsisSuffix.md';
 
 const { Paragraph } = Typography;
 
@@ -10,7 +13,7 @@ const EllipsisSuffix: React.FC = () => {
     "To be, or not to be, that is the question: Whether it is nobler in the mind to suffer. The slings and arrows of outrageous fortune Or to take arms against a sea of troubles, And by opposing end them? To die: to sleep; No more; and by a sleep to say we end The heart-ache and the thousand natural shocks That flesh is heir to, 'tis a consummation Devoutly to be wish'd. To die, to sleep To sleep- perchance to dream: ay, there's the rub! For in that sleep of death what dreams may come When we have shuffled off this mortal coil, Must give us pause. There 's the respect That makes calamity of so long life";
 
   return (
-      <React.Fragment>
+      <DemoCard source={<MD />} attributes={attributes}>
         <Slider value={rows} min={1} max={10} onChange={setRows} />
         <Paragraph
           ellipsis={{
@@ -25,7 +28,7 @@ const EllipsisSuffix: React.FC = () => {
         >
           {article}
         </Paragraph>
-      </React.Fragment>
+      </DemoCard>
   );
 };
 
